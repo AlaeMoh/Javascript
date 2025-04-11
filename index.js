2ad282328b1cbfe1ce0dcff5f68a114ac8e70a71
unknown <alae.muhammad@gmail.com>
2025-04-11 16:40:59 +0200

    project update

--- index.js
+++ index.js
@@ -2,58 +2,67 @@
 
 
 
-let randomMove= Math.random();
+// let randomMove= Math.random();
+// let computerMove='';
+// if( randomMove >=0 &&  randomMove < 1/3){
+//     computerMove= `<i class="fa-solid fa-hand-fist"></i> `;
+// } else if( randomMove >=1/3 &&  randomMove < 2/3){
+//      computerMove= `<i class="fa-solid fa-hand"></i>`;
+// }else if(randomMove >=2/3 && randomMove <1 ){
+//      computerMove= `<i class="fa-solid fa-hand-peace"></i>`;
+// }
 let computerMove='';
-if( randomMove >=0 &&  randomMove < 1/3){
-    computerMove= "Rock";
-} else if( randomMove >=1/3 &&  randomMove < 2/3){
-     computerMove= "Paper";
-}else if(randomMove >=2/3 && randomMove <1 ){
-     computerMove= "Scissors";
-
+function pcMove(){
+    let randomMove= Math.random();
+    if( randomMove >=0 &&  randomMove < 1/3){
+        computerMove= `<i class="fa-solid fa-hand-fist"></i> `;
+    } else if( randomMove >=1/3 &&  randomMove < 2/3){
+        computerMove= `<i class="fa-solid fa-hand"></i>`;
+    }else if(randomMove >=2/3 && randomMove <1 ){
+        computerMove= `<i class="fa-solid fa-hand-peace"></i>`;
+    }
 }
 
-console.log(computerMove);
 
 let result='';
 function button1(){
-    if( computerMove === "Rock"){
+    if( computerMove === `<i class="fa-solid fa-hand-fist"></i>`){
        result= 'It is a tie';
-    }else if( computerMove === "Paper"){
+    }else if( computerMove === `<i class="fa-solid fa-hand"></i>`){
       result= 'You Loooose';
-    } else if( computerMove === "Scissors"){
+    } else if( computerMove === `<i class="fa-solid fa-hand-peace"></i>`){
         result='Check Who is the WINNNEER';
+      
     }
-    alert(`You picked Rock. The computer picked ${computerMove} .${result}
-        ${JSON.stringify(scores)}`)
+    document.querySelector('.js-note').innerHTML=`You  <i class="fa-solid fa-hand-fist"></i>. The computer ${computerMove}`;
+    document.querySelector('.js-result').innerHTML=`${result}`;
+
 
     }
 
     
     function button2(){
-        if( computerMove === "Rock"){
+        if( computerMove === `<i class="fa-solid fa-hand-fist"></i>`){
             result= 'Check Who is the WINNNEER';
-         }else if( computerMove === "Paper"){
+         }else if( computerMove === `<i class="fa-solid fa-hand"></i>`){
            result= 'It is a tie';
-         } else if( computerMove === "Scissors"){
+         } else if( computerMove === `<i class="fa-solid fa-hand-peace"></i>`){
              result='You Loooose';
          }        
-         alert(`You picked Paper. The computer picked ${computerMove} .${result}
-            wins=${scores.wins}, loses=${scores.loses}, ties=${scores.ties}`)
+        document.querySelector('.js-note').innerHTML=`You  <i class="fa-solid fa-hand"></i>. The computer  ${computerMove}`;
 
         }
 
     
     function button3(){
-        if( computerMove === "Rock"){
+        if( computerMove === `<i class="fa-solid fa-hand-fist"></i>`){
             result= 'You Loooose';
-         }else if( computerMove === "Paper"){
+         }else if( computerMove === `<i class="fa-solid fa-hand"></i>`){
            result=  'Check Who is the WINNNEER';
-         } else if( computerMove === "Scissors"){
+         } else if( computerMove === `<i class="fa-solid fa-hand-peace"></i>`){
              result='It is a tie';
          }     
-         alert(`You picked Scissors. The computer picked ${computerMove} .${result}
-            ${JSON.stringify(scores)}`)
+         document.querySelector('.js-note').innerHTML=`You  <i class="fa-solid fa-hand-peace"></i>. The computer  ${computerMove}`;
 
         }
 
@@ -61,8 +70,9 @@ function button1(){
             wins: 0,
             loses: 0,
             ties: 0,
-        
+
         }
+
         let box="";
         
     function score(){
@@ -72,21 +82,21 @@ function button1(){
         } else if (result === 'You Loooose'){
             scores.loses+=1;
         }else if(result=='It is a tie'){
-            scores.ties +=1;
+            scores.ties+=1;
+
         }
-        console.log(scores);
-        box+=`
-        <tr>
-          <td> ${scores.wins}</td>
-          <td>  ${scores.loses} </td>
-          <td> ${scores.ties}</td>
-        </tr>
-        `;
-        document.getElementById("tbody").innerHTML=box;
-    
-     
+
+        document.querySelector('.js-score').innerHTML=`wins:${scores.wins}, loses:  ${scores.loses}, Ties:  ${scores.ties}`;
+        localStorage.setItem('score', JSON.stringify(scores));
+
     }
 
     
+    function reset(){
+        scores.wins=0;
+        scores.ties=0;
+        scores.loses=0;
+        localStorage.removeItem('score');
+    }
     
   
